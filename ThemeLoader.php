f03b29789842239ee8ae48cd215449b169be7fdc
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-27 17:30:01 +0200

    Load homepage assets in themeLoader

--- ThemeLoader.php
+++ ThemeLoader.php
@@ -5,10 +5,17 @@
 		public function __construct(){
 
 			add_action('wp_enqueue_scripts',array($this, 'loadParentStyle'));
+			add_action('wp_enqueue_scripts', array($this, 'loadHomepageAssets'));
 		}
 
 		public function loadParentStyle(){
 			wp_enqueue_style('parent-style', get_template_directory_uri()."/style.css");
+		}
+
+		public function loadHomepageAssets(){
+			if(is_front_page()){
+				wp_enqueue_style('homepage', get_stylesheet_directory_uri()."/assets/css/homepage.css");
+			}
 		}
 	}
 ?>
