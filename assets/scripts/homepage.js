a0e11290b90cdfed84a53b1d5eaad49a00c2d7fa
MathieuBouvet <mathieubouvet42@gmail.com>
2018-09-28 11:46:02 +0200

    Add a test slide fixation function

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -3,5 +3,11 @@
 		var root = $(":root")[0];
 		root.style.setProperty("--menu-height", ($(".navigation-top").height()+2)+"px");
 
+		$(".diapo-slide").click(function(event){
+			var width = $(this).width();
+			$(this).addClass("fixed");
+			$(this).width(width+"px");
+		});
+
 	});
 })(jQuery);
