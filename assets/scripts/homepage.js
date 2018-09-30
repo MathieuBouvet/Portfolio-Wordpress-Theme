89599f07ffa72d4edf868a73c66f7dba8707af08
MathieuBouvet <mathieubouvet42@gmail.com>
2018-10-01 00:06:35 +0200

    remove a test function

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -3,11 +3,7 @@
 		var root = $(":root")[0];
 		root.style.setProperty("--menu-height", ($(".navigation-top").height()+2)+"px");
 
-		$(".diapo-slide").click(function(event){
-			var width = $(this).width();
-			$(this).addClass("fixed");
-			$(this).width(width+"px");
-		});
 
+		
 	});
 })(jQuery);
