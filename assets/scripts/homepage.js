29e0657127b78c8a87bfdaab724997da1037eba0
MathieuBouvet <mathieubouvet42@gmail.com>
2018-10-02 12:36:32 +0200

    Add adminbar calculations

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -3,6 +3,10 @@
 		var root = $(":root")[0];
 		root.style.setProperty("--menu-height", ($(".navigation-top").outerHeight())+"px");
 
+		var adminBar = $("#wpadminbar");
+		if(adminBar.length>0){
+			root.style.setProperty("--admin-panel", adminBar.outerHeight()+"px");
+		}
 
 		
 	});
