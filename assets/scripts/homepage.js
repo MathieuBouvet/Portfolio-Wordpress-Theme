282ab6c72b7cb8832680419adca0510403a8dde5
MathieuBouvet <mathieubouvet42@gmail.com>
2018-10-03 22:25:20 +0200

    Move code in diapo project

--- assets/scripts/homepage.js
+++ assets/scripts/homepage.js
@@ -1,16 +1,6 @@
 (function($){
 	$(function(){
-		var root = $(":root")[0];
-		root.style.setProperty("--menu-height", ($(".navigation-top").outerHeight())+"px");
-
-		var adminBar = $("#wpadminbar");
-		if(adminBar.length>0){
-			root.style.setProperty("--admin-panel", adminBar.outerHeight()+"px");
-		}
-
-		window.addEventListener('resize', function(){
-			root.style.setProperty("--menu-height", $(".navigation-top").outerHeight()+"px");
-		});
+		
 
 		
 	});
