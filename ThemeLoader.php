b61939009e5b6b62ac765c53fd9f44154caadab8
MathieuBouvet <mathieubouvet42@gmail.com>
2018-11-22 13:18:00 +0100

    Load custom css for drawings-archive page

--- ThemeLoader.php
+++ ThemeLoader.php
@@ -6,6 +6,7 @@
 
 			add_action('wp_enqueue_scripts',array($this, 'loadParentStyle'));
 			add_action('wp_enqueue_scripts', array($this, 'loadHomepageAssets'));
+			add_action('wp_enqueue_scripts', array($this, 'loadDrawingsArchiveAssets'));
 		}
 
 		public function loadParentStyle(){
@@ -24,6 +25,11 @@
 				wp_register_script('infinite-scroll', get_stylesheet_directory_uri()."/assets/scripts/lib/infinitescroll.min.js",array('jquery'));
 				
 				wp_enqueue_script('homepage', get_stylesheet_directory_uri()."/assets/scripts/homepage.js",array('jquery','diapositives','infinite-scroll'));
+			}
+		}
+		public function loadDrawingsArchiveAssets(){
+			if(is_post_type_archive(MBPortfolioPlugin::CPT_NAME) && !is_front_page()){
+				wp_enqueue_style('drawings-archive', get_stylesheet_directory_uri()."./assets/css/drawings-archive.css");
 			}
 		}
 	}
